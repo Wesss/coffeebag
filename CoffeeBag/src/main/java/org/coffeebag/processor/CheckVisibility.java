eec80037542a0c1a3d41bcbf5b7e08c18e45a042
Wesley <wesleymcox@gmail.com>
2016-12-02 18:57:38 -0800

    printing using compiler's messager

--- CoffeeBag/src/main/java/org/coffeebag/processor/CheckVisibility.java
+++ CoffeeBag/src/main/java/org/coffeebag/processor/CheckVisibility.java
@@ -2,29 +2,36 @@ package org.coffeebag.processor;
 
 import java.util.Set;
 
-import javax.annotation.processing.AbstractProcessor;
-import javax.annotation.processing.RoundEnvironment;
-import javax.annotation.processing.SupportedAnnotationTypes;
-import javax.annotation.processing.SupportedSourceVersion;
+import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
 
 @SupportedAnnotationTypes("*")
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 public class CheckVisibility extends AbstractProcessor {
 
+	private Messager messager;
+
+	@Override
+	public synchronized void init(ProcessingEnvironment env){
+		super.init(env);
+		this.messager = env.getMessager();
+	}
+
 	@Override
 	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
 		if (!roundEnv.processingOver()) {
-			
 			for (Element element : roundEnv.getRootElements()) {
 				final ReferenceFinder finder = new ReferenceFinder(processingEnv, element);
 				final Set<String> usedTypes = finder.getTypesUsed();
-				System.out.println("Element " + element + " used these types:");
+				String message = "Element " + element + " used these types:";
 				for (String used : usedTypes) {
-					System.out.println("\t" + used);
+					message +="\n\t" + used;
 				}
+				messager.printMessage(Diagnostic.Kind.NOTE, message, element);
+				// messager.printMessage(Diagnostic.Kind.ERROR, "DIE COMPILER, DIE!", element);
 			}
 		}
 		// Allow other annotations to be processed
