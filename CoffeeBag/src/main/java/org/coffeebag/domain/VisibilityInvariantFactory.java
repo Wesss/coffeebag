a7275ff01e6546c0914c87c1a949b920ad43efcf
wesley <wesleymcox@gmail.com>
2016-12-08 16:26:11 -0800

    new classes, forgot to add w/previous commit

---
+++ CoffeeBag/src/main/java/org/coffeebag/domain/VisibilityInvariantFactory.java
@@ -0,0 +1,9 @@
+package org.coffeebag.domain;
+
+public class VisibilityInvariantFactory {
+
+    public static VisibilityInvariant getPublicInvariant() {
+        //TODO
+        return new VisibilityInvariant();
+    }
+}
---
+++ CoffeeBag/src/main/java/org/coffeebag/processor/invariants/InvariantScanner.java
@@ -0,0 +1,28 @@
+package org.coffeebag.processor.invariants;
+
+import org.coffeebag.annotations.Visibility;
+import org.coffeebag.log.Log;
+
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.ElementScanner8;
+
+/**
+ * Scans the element tree to look for annotated elements
+ */
+public class InvariantScanner extends ElementScanner8<Void, Void> {
+
+    private static String TAG = InvariantScanner.class.getSimpleName();
+
+    private InvariantFinder invariantFinder;
+
+    public InvariantScanner(InvariantFinder invariantFinder) {
+        this.invariantFinder = invariantFinder;
+    }
+
+    @Override
+    public Void visitType(TypeElement e, Void p) {
+        Log.d(TAG, "Visiting type " + e);
+        invariantFinder.storeInvariant(e, Visibility.PUBLIC); //TODO
+        return super.visitType(e, p);
+    }
+}
