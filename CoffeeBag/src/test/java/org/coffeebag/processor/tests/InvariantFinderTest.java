b0d7e9284a89e3841714471dbc62342239bfefa7
Sam Crow <samcrow@uw.edu>
2016-12-10 21:23:48 -0800

    Ignored invariant finder tests

--- CoffeeBag/src/test/java/org/coffeebag/processor/tests/InvariantFinderTest.java
+++ CoffeeBag/src/test/java/org/coffeebag/processor/tests/InvariantFinderTest.java
@@ -1,8 +1,10 @@
 package org.coffeebag.processor.tests;
 
 import org.coffeebag.processor.InvariantFinderRunner;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
+@Ignore
 @RunWith(InvariantFinderRunner.class)
 public class InvariantFinderTest {
 
