46b1b0b63ec513a5ac88647a1e4f31072af3a68a
Wesley <wesleymcox@gmail.com>
2016-12-11 00:04:53 -0800

    Subclass visibility tests

---
+++ CoffeeBag/test-data/VisibilityChecker/IllegalSubclassVisibilityUsedByOther/ClassA.java
@@ -0,0 +1,10 @@
+package org.local.util;
+
+import org.coffeebag.annotations.Visibility;
+import org.coffeebag.annotations.Access;
+
+@Access(level = Visibility.SUBCLASS)
+public class ClassA {
+
+}
+
---
+++ CoffeeBag/test-data/VisibilityChecker/IllegalSubclassVisibilityUsedByOther/ClassB.java
@@ -0,0 +1,12 @@
+package com.example.company;
+
+import org.local.util.ClassA;
+
+public class ClassB {
+
+	private static ClassA someReference;
+
+	public ClassB() {
+		someReference = new ClassA();
+	}
+}
