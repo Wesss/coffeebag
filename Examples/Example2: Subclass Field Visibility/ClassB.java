a0f0b8bf0b16426095ac0cd4eba79258e18cf281
Wesley <wesleymcox@gmail.com>
2016-12-12 12:23:33 -0800

    readme, examples skeletons

---
+++ Examples/Example2: Subclass Field Visibility/ClassB.java
@@ -0,0 +1,12 @@
+package com.example.company;
+
+import org.local.util.ClassA;
+
+public class ClassB {
+
+	public static ClassA someReference;
+
+	public ClassB() {
+		someReference = new ClassA();
+	}
+}
